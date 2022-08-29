2858e59ac390fdaf132ad72c60e9c0ca7a6fe645
Hiram Estrada <52044054+HiramEs@users.noreply.github.com>
2022-08-29 00:41:53 -0500

    device info

--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,14 +1,25 @@
 import {View, Text, StyleSheet} from 'react-native';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import CustomTextInput from '../components/TextInput';
 import ProfileImage from '../components/ProfileImage';
 import {ScrollView} from 'react-native-gesture-handler';
+import {getDevice, getDeviceName} from 'react-native-device-info';
 
 export default function ProfileScreen() {
   const [name, setName] = useState<string>('Kaladin Stormblessed');
   const [username, setUserName] = useState<string>('Kal4');
   const [contact, setContact] = useState<string>('4494442344');
   const [age, setAge] = useState<string>('23');
+  const [device, setDevice] = useState<string>('');
+
+  const getDeviceInfo = async () => {
+    const deviceInfo = await getDeviceName();
+    setDevice(deviceInfo);
+  };
+
+  useEffect(() => {
+    getDeviceInfo();
+  });
 
   return (
     <View style={styles.container}>
@@ -40,6 +51,10 @@ export default function ProfileScreen() {
           onChangeText={value => setAge(value)}
           keyboard="numeric"
         />
+        <View style={styles.deviceInfo}>
+          <Text style={styles.infoLabel}>Device Name: </Text>
+          <Text style={styles.text}>{device}</Text>
+        </View>
       </ScrollView>
     </View>
   );
@@ -49,5 +64,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#000000',
+  },
+  deviceInfo: {
+    flexDirection: 'row',
+    alignSelf: 'center',
+  },
+  infoLabel: {
+    color: '#ffffff',
+    fontWeight: '800',
+    fontSize: 16,
+  },
+  text: {
+    color: '#ffffff',
+    fontSize: 16,
   },
 });
